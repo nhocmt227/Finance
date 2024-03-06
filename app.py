1279fdf270e3cbd232e3e42d3c72e49508932a28
nhocmt227 <phong1572005@gmail.com>
2024-03-06 14:51:00 +0800

    temporary commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
+import sqlite3
 
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -19,7 +19,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+con = sqlite3.connect("tutorial.db")
+db = con.cursor()
 
 
 @app.after_request
